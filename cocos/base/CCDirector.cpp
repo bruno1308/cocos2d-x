9e13e4cddded75b90b66a55f988d478bb014e221
Felippe Durán <felippeduran@MacBook-Pro.local>
2016-08-24 18:54:15 -0300

    Add transition feature to popScene().

--- cocos/base/CCDirector.cpp
+++ cocos/base/CCDirector.cpp
@@ -255,6 +255,13 @@ void Director::setGLDefaultValues()
     setProjection(_projection);
 }
 
+Scene* Director::previousScene(void) {
+    unsigned int c = _scenesStack.size();
+    
+    if (c <= 1) return NULL;
+    return (Scene *) _scenesStack.at(c - 2);
+}
+
 // Draw the Scene
 void Director::drawScene()
 {
@@ -872,6 +879,21 @@ void Director::popScene(void)
     {
         _sendCleanupToScene = true;
         _nextScene = _scenesStack.at(c - 1);
+    }
+}
+
+void Director::popScene(Scene *trans) {
+    CCAssert(_runningScene != NULL, "running scene should not null");
+    _scenesStack.popBack();
+    unsigned int c = _scenesStack.size();
+    
+    if (c == 0) {
+        end();
+    }
+    else {
+        
+        _sendCleanupToScene = true;
+        _nextScene = trans;
     }
 }
 
